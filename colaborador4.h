15e2c827bf5665268241d21caa99e54dd38c899e
Heidysaldarriaga <86266307+Heidysaldarriaga@users.noreply.github.com>
2021-07-06 16:23:53 -0500

    Update colaborador4.h
    Yo modifiqué el código para mostrar la transpuesta de la matriz 1, para ello utilicé condicionales for para que se encarguen de contar los términos de la matriz y en el mensaje de presentar invertí las filas con las columnas para que resulte la transpuesta de la matriz

--- colaborador4.h
+++ colaborador4.h
@@ -3,7 +3,14 @@ void transpuesta(float (*m1)[5], float (*r)[5])
 
 cout<<"Obtener la transpuesta de una matriz";
 
-
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		
+		r[j][i]=m1[j][i];
+		cout<<r[j][i]<<" ";
+	}
+	cout<<"\n";
+}
 
 
 }
