09ae4986600eb4b8e4ba3aaa718fe610e7b663a2
ana409 <86269965+ana409@users.noreply.github.com>
2021-07-06 16:49:37 -0500

    Update colaborador3.h
    Hice la modificación, para presentar la elevación al cuadrado de la primera matriz utilizando  ciclos for para contar las columnas y las filas de la matriz. La elevación al cuadrado la hice multiplicando dos veces la matriz1 .

--- colaborador3.h
+++ colaborador3.h
@@ -2,7 +2,14 @@ void potencia(float (*m1)[5], int p, float (*r)[5])
 {
 
 cout<<"Calcular la potencia p de una ";
+for(int i=0;i<5;i++)
+for(int j=0;j<5;j++)
+r[i][j]=0;
+for(int i=0;i<5;i++)
+for(int j=0;j<5;j++)
+for(int k=0;k<5;k++)
 
+r[i][j]+= m1[i][k]*m1[k][j];
 
 
 
