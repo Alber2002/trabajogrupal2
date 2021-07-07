7daa4623f25062d2e186d3b79b233ca52a8a3487
MayerliParraga <86315523+MayerliParraga@users.noreply.github.com>
2021-07-07 08:52:23 -0500

    Update colaborador1.h
    realicé la modificación de este código  para pedirle usuario que digite los valores de las matrices y posteriormente presentarlas, desarrollé el código con ciclos for para almacenar los valores que digitó el usuario en primeria instancia, y después utilicé otros ciclos for para ya presentar las matrices

--- colaborador1.h
+++ colaborador1.h
@@ -1,9 +1,41 @@
 void ingresar(float (*m1)[5], float (*m2)[5])
 {
 
-cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
+cout<<"\n\nIngresar y mostrar los valores de los arreglos o matrices";
 
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<"Digite un numero ["<<i<<"]["<<j<<"]";
+	      cin>>*(*(m1+i)+j);
+	}
+}
+	cout<<endl;
+cout<<"Ingresando la segunda matriz"<<endl<<endl;
+		
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<"Digite un numero ["<<i<<"]["<<j<<"]";
+	      cin>>*(*(m2+i)+j);
+	}
+}
+}
 
+void mostrar (float (*m1)[5], float (*m2)[5]){
+	
+	for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m1[ i ] [ j ];
+	}
+}
+	cout<<endl;
+	
+	for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m2[i][j];
+	}
+	cout<<endl;
+	
+}
 
 
 }
