4347c329d14cb50ca9bfd3ec4524b8ce41c81ea4
GenesisGallegos <86111820+GenesisGallegos@users.noreply.github.com>
2021-07-07 08:45:42 -0500

    Update colaborador2.h
    modifiqué el código para hallar el producto de las dos matrices, para ello utilicé ciclos for para contar las filas y las columnas de la primera y segunda matriz, declaré una nueva variable y ciclo for para poder igualar las columnas de la primera matriz y las filas de la segunda, después realicé la multiplicación entre las dos matrices colocándole la tercera variable en las columnas de la primera y filas de la segunda para poder obtener el resultado

--- colaborador2.h
+++ colaborador2.h
@@ -1,9 +1,16 @@
 void producto(float (*m1)[5], float (*m2)[5], float (*r)[5])
 {
 
-cout<<"Obetener el producto de las dos matrices ";
+cout<<"Obetener el producto de las dos matrices\n\n";
 
+for(int i=0;i<5;i++)
+for(int j=0;j<5;j++)
+r[i][j]=0;
+for(int i=0;i<5;i++)
+for(int j=0;j<5;j++)
+for(int k=0;k<5;k++)
 
+r[i][j]+= m1[i][k]*m2[k][j];
 
 
 }
